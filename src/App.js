26ce5d837baad014cf21ce567e7f57476683ffc0
mpcarolin <mpcarolin.dev@gmail.com>
2020-08-25 17:05:31 -0700

    updated to use keg-hub evf package, and pass in mocked out data to demonstrate how to use the component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Sessions from '@simpleviewinc/tap-events-force'
+import Sessions from '@keg-hub/tap-evf-sessions'
+import testData from './mocks/testData'
 // For CJS: import Sessions from '@simpleviewinc/tap-events-force/keg-sessions.cjs'
 
 function App() {
-  return <Sessions />
+  return <Sessions {...testData} />
 }
 
 export default App;
