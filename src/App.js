232ae495c5fb0f29f0f6059c0110d5fd6306951b
mpcarolin <mpcarolin.dev@gmail.com>
2020-09-08 17:44:33 -0700

    updated version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sessions from '@keg-hub/tap-evf-sessions'
 import testData from './mocks/testData'
-// For CJS: import Sessions from '@simpleviewinc/tap-events-force/keg-sessions.cjs'
 
 function App() {
-  return <Sessions {...testData} />
+  return <Sessions 
+    disableDemo={false} 
+    {...testData} 
+  />
 }
 
 export default App;
