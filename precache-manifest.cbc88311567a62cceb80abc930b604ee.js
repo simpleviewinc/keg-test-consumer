800f92f1fb57f139f8da325f1a963a1fe9d43846
mpcarolin <mpcarolin.dev@gmail.com>
2020-10-22 12:49:04 -0700

    Updates

---
+++ precache-manifest.cbc88311567a62cceb80abc930b604ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134b741b934800c3cf2f08846547da57",
+    "url": "/keg-test-consumer/index.html"
+  },
+  {
+    "revision": "e2e90653ccc7adc826b1",
+    "url": "/keg-test-consumer/static/js/2.ad932fce.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/keg-test-consumer/static/js/2.ad932fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f807c8716a7b7f4b963",
+    "url": "/keg-test-consumer/static/js/main.3c69b7c2.chunk.js"
+  },
+  {
+    "revision": "2f9c3ed244c78da87cee",
+    "url": "/keg-test-consumer/static/js/runtime-main.25a7078b.js"
+  }
+]);
