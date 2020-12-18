02ae5ad4f95f88537833d15f79855ba086e444f3
Daniel Yo <dnl992004@yahoo.com.au>
2020-12-17 17:06:16 -0700

    Updates

--- precache-manifest.c33ba75ff3ace959308473ba9e8af693.js
+++ precache-manifest.c33ba75ff3ace959308473ba9e8af693.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3c85190dc627145581917e438422ceb4",
+    "revision": "23101dbbc87bd1c6a05519dcc1bc4beb",
     "url": "/keg-test-consumer/index.html"
   },
   {
-    "revision": "4cb175fc1ef51035f3d4",
+    "revision": "baefae27194c37ab2245",
     "url": "/keg-test-consumer/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "4cb175fc1ef51035f3d4",
-    "url": "/keg-test-consumer/static/js/2.b83b45a2.chunk.js"
+    "revision": "baefae27194c37ab2245",
+    "url": "/keg-test-consumer/static/js/2.f9104bac.chunk.js"
   },
   {
     "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
-    "url": "/keg-test-consumer/static/js/2.b83b45a2.chunk.js.LICENSE.txt"
+    "url": "/keg-test-consumer/static/js/2.f9104bac.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "feb748119e96b2fd4cf8",
-    "url": "/keg-test-consumer/static/js/main.480bf034.chunk.js"
+    "revision": "80cdddf282e7ea19fdff",
+    "url": "/keg-test-consumer/static/js/main.bd283213.chunk.js"
   },
   {
     "revision": "2f9c3ed244c78da87cee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/keg-test-consumer/precache-manifest.e13a7a2e7d07ce6ee9921a824faf9510.js"
+  "/keg-test-consumer/precache-manifest.c33ba75ff3ace959308473ba9e8af693.js"
 );
 
 self.addEventListener('message', (event) => {
