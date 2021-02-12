b72cec6d76bdc725fc34d1b14c7036269ef4aff3
Daniel Yo <dnl992004@yahoo.com.au>
2021-02-12 10:15:15 -0700

    Updates

--- precache-manifest.7b70d79dad2022ba39e8bc791b25c1e9.js
+++ precache-manifest.7b70d79dad2022ba39e8bc791b25c1e9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "45217495bdeb581877a57469282e3f26",
+    "revision": "4e59937528226b6f4427d1dfeadbbdd0",
     "url": "/keg-test-consumer/index.html"
   },
   {
-    "revision": "267c476a463242d527e6",
+    "revision": "2f4888fe47e764fa7e95",
     "url": "/keg-test-consumer/static/css/2.95258f03.chunk.css"
   },
   {
-    "revision": "267c476a463242d527e6",
-    "url": "/keg-test-consumer/static/js/2.15c76f1a.chunk.js"
+    "revision": "2f4888fe47e764fa7e95",
+    "url": "/keg-test-consumer/static/js/2.cb55aa44.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/keg-test-consumer/static/js/2.15c76f1a.chunk.js.LICENSE.txt"
+    "url": "/keg-test-consumer/static/js/2.cb55aa44.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "16aa55fd6827de0f1406",
-    "url": "/keg-test-consumer/static/js/main.5748185b.chunk.js"
+    "revision": "bbdc13e4aa4267bd277a",
+    "url": "/keg-test-consumer/static/js/main.e3a75c2c.chunk.js"
   },
   {
     "revision": "2f9c3ed244c78da87cee",
